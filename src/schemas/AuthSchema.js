604255f55e6bb0f9ca205d226e502e0779c2f895
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-22 12:13:06 -0300

    fix: authentication schema

--- src/schemas/AuthSchema.js
+++ src/schemas/AuthSchema.js
@@ -1,13 +1,13 @@
 import joi from 'joi'
 
 export const signupSchema = joi.object({
-    name: joi.string().required(),
+    name: joi.string().min(3).required(),
     email: joi.string().email().required(),
-    password: joi.string().required(),
+    password: joi.string().min(3).required(),
     confirmPassword: joi.string().valid(joi.ref('password')).required()
 });
 
 export const signinSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().required(),
+    password: joi.string().min(3).required(),
 });
