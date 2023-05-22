f29d8d41437938bd060136c7cefb7bb6e7cef8a1
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-22 14:38:11 -0300

    feat: add middleware to get urls by user

--- src/middlewares/UserMiddleware.js
+++ src/middlewares/UserMiddleware.js
@@ -1,4 +1,3 @@
-import { getUrlsByUser } from "../repositories/repository.js";
 import bcrypt from "bcrypt";
 
 export async function validateSignup(req, res, next) {
@@ -32,3 +31,32 @@ export async function validateSignin(req, res, next) {
   }
 }
 
+export async function getUrlsByUser(req, res, next) {
+  const { userId } = res.locals.session;
+
+  const urls = await db.query(`
+    SELECT
+    json_build_object(
+      'id', "shortLinks"."userId",
+      'name', users.name,
+      'visitCount', SUM("shortLinks"."visitCount"),
+      'shortenedUrls', json_agg(
+        json_build_object(
+          'id', "shortLinks".id,
+          'shortUrl', "shortLinks"."shortUrl",
+          'url', "shortLinks".url,
+          'visitCount', "shortLinks"."visitCount"
+        )
+      )
+    ) AS user
+    FROM "shortLinks"
+    JOIN users ON users.id = "shortLinks"."userId"
+    WHERE "shortLinks"."userId" = $1
+    GROUP BY "shortLinks"."userId", users.name
+    ORDER BY SUM("shortLinks"."visitCount");`
+      , [userId]);
+
+  res.locals.urls = urls.rows;
+  
+  next();
+}
--- src/routers/UsersRouter.js
+++ src/routers/UsersRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUserData, getUsersRanking } from "../controllers/users.controller.js";
 import { authValidation } from "../middlewares/auth.middleware.js";
+import { getUrlsByUser } from "../middlewares/UserMiddleware.js";
 
 const usersDataRouter = Router()
 
-usersDataRouter.get("/users/me", authValidation, getUserData)
+usersDataRouter.get("/users/me", authValidation, getUrlsByUser, getUserData)
 usersDataRouter.get("/ranking", getUsersRanking)
 
 export default usersDataRouter
