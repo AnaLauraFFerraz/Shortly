360b5be514aede9f62158b5a61f5e8f0bb749620
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-23 13:24:24 -0300

    fix: remove redundant code

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,17 +6,12 @@ export async function signUp(req, res) {
     const { name, email, password } = req.body
     const hashedPassword = bcrypt.hashSync(password, 10)
     try {
-        const {rowCount: hasEmail} = await db.query("SELECT email FROM users where email = $1;",[email])
-        
-        if (hasEmail) return res.status(409).send("Email already exists")
-
         const user = await db.query(`
-        INSERT INTO users (name,email,password)
-        VALUES ($1,$2,$3)`,
-        [name, email, hashedPassword])
+            INSERT INTO users (name,email,password)
+            VALUES ($1,$2,$3)`,
+            [name, email, hashedPassword])
 
         res.sendStatus(201)
-
     } catch (err) {
         res.status(500).send(err.message)
     }
